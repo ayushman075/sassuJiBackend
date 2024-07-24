89dca21e5d69d4eaa62351e2ef86c0a040eb2d8c
Rishabh <rishabh09p@gmail.com>
2024-07-25 01:28:49 +0530

    Fixes

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -88,8 +88,7 @@ const updateProduct =asyncHandler( async (req, res) => {
 
 
   const getProductById = asyncHandler( async (req, res) => {
-    const { id } = req.params;
-  
+    const { id } = req.query;
     const product = await Product.findById(id).populate('seller', 'username email');
   
     if (!product) {
